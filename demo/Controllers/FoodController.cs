4577921416316054f70d1dbd183bd9542bb32934
HP <HP@LAPTOP-27BS1SL4>
2026-03-24 12:25:58 +0700

    Add project files.

---
+++ demo/Controllers/FoodController.cs
@@ -0,0 +1,31 @@
+﻿using demo.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FoodController : ControllerBase
+    {
+        static List<Food> foods = new List<Food>()
+        {
+            new Food { Id = 1, Name = "Pho", Calories = 350 },
+            new Food { Id = 2, Name = "Banh Mi", Calories = 250 }
+        };
+
+        // GET: api/food
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(foods); // trả về JSON
+        }
+
+        // POST: api/food
+        [HttpPost]
+        public IActionResult Create(Food food)
+        {
+            foods.Add(food);
+            return Ok(food); // nhận + trả JSON
+        }
+    }
+}
---
+++ demo/Model/Food.cs
@@ -0,0 +1,9 @@
+﻿namespace demo.Model
+{
+    public class Food
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Calories { get; set; }
+    }
+}
